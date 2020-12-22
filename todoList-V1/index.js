a572f71eb4c5d2a879cc32454c8fb802ffd01c1f
Ali Haider <alihaider589@outlook.com>
2020-12-22 12:59:38 +0500

    Adding changes

--- todoList-V1/index.js
+++ todoList-V1/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const items = ["Buy Food","Cook Food","Eat Food"]
+const items = ["Buy Food", "Cook Food", "Eat Food"]
+const workItems = [];
 
 const app = express();
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     var today = new Date()
@@ -19,19 +21,38 @@ app.get('/', (req, res) => {
 
 
 
-    res.render('list', { kindofday: day, newListItems: items })
+    res.render('list', { ListTitle: day, newListItems: items })
 
 })
 
 app.post('/', (req, res) => {
     var item = req.body.todo
 
+    if(req.body.list=='WorkList'){
+        workItems.push(item)
+        res.redirect('/work')
 
-    items.push(item)
+    }else{
+       items.push(item)
+       res.redirect('/') 
+    }
+  
 
-    res.redirect('/')
+  
 })
 
+app.get('/about',(req,res)=>{
+res.render('about')
+})
+app.get('/work', (req, res) => {
+    res.render('list', { ListTitle: 'WorkList', newListItems: workItems })
+
+})
+app.post('/work', (req, res) => {
+    var item = req.body.todo
+    workItems.push(item)
+
+})
 
 app.listen(3000, () => {
     console.log('App is running on port 3000')
